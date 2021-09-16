4ec633ffb6771c47146db8c9102bd07383cfdbb1
lucas997 <lucasgojer@gmail.com>
2021-09-16 11:42:52 -0300

    atualizando os exercícios

---
+++ medidor_velocidade.js
@@ -0,0 +1,25 @@
+// velocidade máxima de até 70
+// a cada 5km acima do limite você ganha 1 ponto
+// Math.Floor();
+// caso pontos sejam maior que 12 -> "Carteira Suspendida"
+
+verificarVelocidade(130);
+
+function verificarVelocidade(velocidade) {
+    const velocidadeMaxima = 70;
+    const kmPorPonto = 5;
+
+    if(velocidade <= velocidadeMaxima) {
+        console.log("Ok!");
+
+    } else {
+        const pontos = Math.floor((velocidade - velocidadeMaxima) / kmPorPonto);
+
+        if(pontos >= 12) {
+            console.log("Carteira Suspensa!")
+
+        } else {
+            console.log("pontos",pontos)
+        }
+    }
+}

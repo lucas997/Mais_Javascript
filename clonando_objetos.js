021f10a31e45f1c3d58aafe72032883ba88abaac
Lucas André <44985462+lucas997@users.noreply.github.com>
2021-09-21 12:09:07 -0300

    Update clonando_objetos.js

--- clonando_objetos.js
+++ clonando_objetos.js
@@ -1,26 +1,5 @@
 // clonando objetos
 
-/* const celular = {
-    marcaCelular: 'ASUS',
-    tamanhoTela: {
-        vertical: 155,
-        horizontal: 75
-    },
-    ligar: function () {
-        console.log("fazendo uma ligação");
-    }
-}
-
-const novoObjeto = Object.assign({
-      bateria: 5000  
-},celular);
-console.log(novoObjeto);
-
-const objeto2 = {...celular};
-console.log(objeto2); 
-
-*/
-
 const time = {
     nome: "Sport",
     estado: "Pernambuco",

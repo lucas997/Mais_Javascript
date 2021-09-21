ad1cf4d3b55a0aa5739ea8b9f3cf62e02414a65f
Lucas André <44985462+lucas997@users.noreply.github.com>
2021-09-21 12:10:18 -0300

    Update for_in.js

--- for_in.js
+++ for_in.js
@@ -3,34 +3,6 @@
 // do-while
 // for-in
 
-/* const pessoa = {
-    nome: "Lucas",
-    idade: 25
-}; */
-
-// key-value
-/* for(let chave in pessoa) {
-    console.log(chave,pessoa['nome']);
-} */
-
- //const cores = ['vermelho', 'azul', 'verde'];
-
-/* 
-
-for(let indice in cores) {
-    console.log(indice,cores[indice]);
-} 
-
-*/
-
-// for-of
-
-/* 
-for(let cor of cores) {
-    console.log(cor);
-}
-*/
-
 const bandas = ['Angra', 'Vision divine', 'Symphony X', 'DragonForce'];
 
 // for-of
